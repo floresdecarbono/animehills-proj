ab91a481121c4a14a95f7867d449a36c796ff953
Peterson William <petersondev.profissional@gmail.com>
2026-03-03 20:14:40 -0300

    adicionando página de artigo para exibir conteúdo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './assets/css/App.css';
 import Header from './Header';
 import HomeContainer from './home/HomeContainer.jsx';
+import { Routes, Route } from 'react-router-dom';
+import { Artigo } from './pages/artigo/Artigo.jsx';
+import { Container } from './components/layout/Container/Container.jsx';
 
 function App() {
  return (
   <>
     <Header/>
-    <HomeContainer/>
+    <Container>
+      <Routes>
+        <Route path="/" element={<HomeContainer/>} />
+        <Route path="/artigo/:id" element={<Artigo />}/>
+      </Routes>
+    </Container>
   </>
  );
 }
--- src/pages/home/HomeContainer.jsx
+++ src/pages/home/HomeContainer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Container = () => {
   return (
     <div id='home-container'>
+      <h1>Olá</h1>
     </div>
   )
 }
